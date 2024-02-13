1a9382a980080fd229c52f5d140d72713789ebf5
Sara S Sparrer <saraahso@gmail.com>
2024-02-13 18:21:08 +0100

    feat:Adds Contact routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,55 +1,105 @@
+import { LinksFunction, json, redirect } from "@remix-run/node";
 import {
-  Form,
-  Links,
-  LiveReload,
-  Meta,
-  Scripts,
-  ScrollRestoration,
+	Form,
+	Link,
+	Links,
+	LiveReload,
+	Meta,
+	NavLink,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
+	useNavigation,
 } from "@remix-run/react";
+import appStylesHref from "./app.css";
+import { createEmptyContact, getContacts } from "./data";
+
+export const links: LinksFunction = () => [
+	{ rel: "stylesheet", href: appStylesHref },
+];
+
+export const action = async () => {
+	const contact = await createEmptyContact();
+	return redirect(`/contacts/${contact.id}/edit`);
+};
+
+export const loader = async () => {
+	const contacts = await getContacts();
+	return json({ contacts });
+};
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+	const { contacts } = useLoaderData<typeof loader>();
+	const navigation = useNavigation();
 
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<div id="sidebar">
+					<h1>Remix Contacts</h1>
+					<div>
+						<Form id="search-form" role="search">
+							<input
+								id="q"
+								aria-label="Search contacts"
+								placeholder="Search"
+								type="search"
+								name="q"
+							/>
+							<div id="search-spinner" aria-hidden hidden={true} />
+						</Form>
+						<Form method="post">
+							<button type="submit">New</button>
+						</Form>
+					</div>
+					<nav>
+						{contacts.length ? (
+							<ul>
+								{contacts.map((contact) => (
+									<li key={contact.id}>
+										<NavLink
+											className={({ isActive, isPending }) =>
+												isActive ? "active" : isPending ? "pending" : ""
+											}
+											to={`contacts/${contact.id}`}
+										>
+											{contact.first || contact.last ? (
+												<>
+													{contact.first} {contact.last}
+												</>
+											) : (
+												<i>No Name</i>
+											)}{" "}
+											{contact.favorite ? <span>★</span> : null}
+										</NavLink>
+									</li>
+								))}
+							</ul>
+						) : (
+							<p>
+								<i>No contacts</i>
+							</p>
+						)}
+					</nav>
+				</div>
+				<div
+					id="detail"
+					className={navigation.state === "loading" ? "loading" : ""}
+				>
+					<Outlet />
+				</div>
+
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
